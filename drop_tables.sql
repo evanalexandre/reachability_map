ef7685b80bddc9b8e4621fdfda1b9f741a37cbf0
evanalexandre <evanalexandre@gmail.com>
2021-10-29 16:47:41 -0400

    added ipv4_addresses table to schema

--- drop_tables.sql
+++ drop_tables.sql
@@ -1,2 +1,3 @@
 DROP TABLE IF EXISTS hosts;
 DROP TABLE IF EXISTS scans;
+DROP TABLE IF EXISTS ipv4_addresses;
--- schema.sql
+++ schema.sql
@@ -1,3 +1,9 @@
+CREATE TABLE IF NOT EXISTS ipv4_addresses (
+    ipv4_id int NOT NULL AUTO_INCREMENT,
+    ipv4_address varchar(255) UNIQUE,
+    PRIMARY KEY (ipv4_id)
+);
+
 CREATE TABLE IF NOT EXISTS scans (
     scan_id int NOT NULL AUTO_INCREMENT,
     start_time timestamp,
@@ -5,17 +11,19 @@ CREATE TABLE IF NOT EXISTS scans (
     command varchar(255),
     uphosts int,
     downhosts int,
-    source_ip varchar(255),
-    PRIMARY KEY (scan_id)
+    source_ipv4_id int,
+    PRIMARY KEY (scan_id),
+    FOREIGN KEY (source_ipv4_id) REFERENCES ipv4_addresses(ipv4_id)
 );
 
 CREATE TABLE IF NOT EXISTS hosts (
     host_id int NOT NULL AUTO_INCREMENT,
     scan_id int NOT NULL,
-    ipv4_address varchar(255),
+    ipv4_id int,
     hostname varchar(255),
     status varchar(255),
     reason varchar(255),
     PRIMARY KEY (host_id),
-    FOREIGN KEY (scan_id) REFERENCES scans(scan_id)
+    FOREIGN KEY (scan_id) REFERENCES scans(scan_id),
+    FOREIGN KEY (ipv4_id) REFERENCES ipv4_addresses(ipv4_id)
 );
