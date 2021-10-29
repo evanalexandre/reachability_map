b742c93ab004a59f780eeac1fb0b98f7c2cd0d29
Evan <evanalexandre@gmail.com>
2021-10-28 22:14:21 -0400

    updated requirements

--- prefix_scraper.py
+++ prefix_scraper.py
@@ -1,22 +1,19 @@
 import netaddr
 import nmap
 import requests
-import datetime
 import config
 
 ASN = config.ASN
 
 
 def get_prefixes(asn):
-    start = datetime.datetime.now()
+    """Returns the list of prefixes orignated from an ASN"""
     prefixes_api = 'https://api.shadowserver.org/net/asn?prefix='
     url = prefixes_api + asn
     result = requests.get(url)
     prefixes = [str(i) for i in result.json()]
     summarized_prefixes = netaddr.cidr_merge(prefixes)
     prefix_strings = [str(i) for i in summarized_prefixes]
-    end = datetime.datetime.now()
-    runtime = start - end
     return(prefix_strings)
 
 
