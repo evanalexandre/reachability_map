779f56a44c327eeac39019c98309db7ee6a77454
Evan <evanalexandre@gmail.com>
2021-10-29 05:14:37 -0400

    added scan_scraper

---
+++ scan_scraper.py
@@ -0,0 +1,15 @@
+import nmap
+import prefix_scraper
+
+
+def ping_sweep(network):
+    scanner = nmap.PortScanner()
+    arguments = '-sP'
+    result = scanner.scan(hosts=network, arguments=arguments)
+    print(result)
+
+
+if __name__ == '__main__':
+    prefixes = prefix_scraper.get_prefixes(prefix_scraper.ASN)
+    for prefix in prefixes:
+        ping_sweep(prefix)
