45e1ba7c867b77f1edcc187b1964b0de628424dc
Evan <evanalexandre@gmail.com>
2021-10-29 07:27:39 -0400

    added hosts table

--- schema.sql
+++ schema.sql
@@ -7,3 +7,14 @@ CREATE TABLE IF NOT EXISTS scans (
     downhosts int,
     PRIMARY KEY (scan_id)
 );
+
+CREATE TABLE IF NOT EXISTS hosts (
+    host_id int NOT NULL AUTO_INCREMENT,
+    scan_id int NOT NULL,
+    ipv4_address varchar(255),
+    hostname varchar(255),
+    status varchar(255),
+    reason varchar(255),
+    PRIMARY KEY (host_id),
+    FOREIGN KEY (scan_id) REFERENCES scans(scan_id)
+);
