bfe8aebcc317d9515689180503f80026c15e3b98
Evan <evanalexandre@gmail.com>
2021-10-29 06:30:56 -0400

    test connection

--- db_connect.py
+++ db_connect.py
@@ -5,8 +5,13 @@ import mysql.connector
 db = mysql.connector.connect(
     host=config.DB_HOST,
     user=config.DB_USER,
-    password=config.DB_PASSWORD
+    password=config.DB_PASSWORD,
+    database=config.DB_NAME
 )
 
+
 if __name__ == '__main__':
-    print(db)
+    cursor = db.cursor()
+    cursor.execute('SHOW TABLES')
+    for i in cursor:
+        print(i)
