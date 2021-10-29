6e4ab8f7ffac0294d0f8063515db967711dc1413
Evan <evanalexandre@gmail.com>
2021-10-29 07:33:20 -0400

    added start time to ping sweep

--- scan_scraper.py
+++ scan_scraper.py
@@ -1,11 +1,14 @@
 import nmap
 import prefix_scraper
+import time
 
 
 def ping_sweep(network):
+    start_time = time.strftime('%Y-%m-%d %H:%M:%S')
     scanner = nmap.PortScanner()
     arguments = '-sP'
     result = scanner.scan(hosts=network, arguments=arguments)
+    result['start_time'] = start_time
     return(result)
 
 
