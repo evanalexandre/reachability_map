5b6860800287a498aa513d37829369c81b04722a
Evan <evanalexandre@gmail.com>
2021-10-28 22:08:31 -0400

    added runtime

--- prefix_scraper.py
+++ prefix_scraper.py
@@ -1,13 +1,17 @@
 import netaddr
 import nmap
 import requests
+import datetime
 
 
 def get_prefixes(asn):
+    start = datetime.datetime.now()
     prefixes_api = 'https://api.shadowserver.org/net/asn?prefix='
     url = prefixes_api + asn
     result = requests.get(url)
     prefixes = [str(i) for i in result.json()]
     summarized_prefixes = netaddr.cidr_merge(prefixes)
     prefix_strings = [str(i) for i in summarized_prefixes]
+    end = datetime.datetime.now()
+    runtime = start - end
     return(prefix_strings)
